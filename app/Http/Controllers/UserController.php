e23470eff99ae84986e33eb64a7de19d8dd4c807
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-04-24 09:35:21 -0400

    cambios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Exports\UsersExport;
 use App\User;
-use App\PDF;
+use PDF;
 use Excel;
 
 class UserController extends Controller
